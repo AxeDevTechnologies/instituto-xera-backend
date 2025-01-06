93af8d8083378ebf5fe490ff81e976878b9b12bf
AxeDev Technologies <axedevtechnologies@gmail.com>
2025-01-06 15:58:07 -0600

    Implement userModel with firestore database

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,33 +1,36 @@
-import { ResultSetHeader, RowDataPacket } from 'mysql2';
-import pool from '../db';
+import { db } from '../firebase/config';
+import { collection, getDocs, addDoc, query, where } from 'firebase/firestore';
 
 export default class UserModel {
-    public static async getUser(email: string): Promise<RowDataPacket[]> {
-        const [result] = await pool.query<RowDataPacket[]>('SELECT * FROM User JOIN Person on User.id_person = Person.id_person WHERE User.email = ?', [email]);
-        return result;
+    public static async getUser(email: string){
+        try {
+            const userQuery = query(collection(db, 'User'), where('email', '==', email));
+            const userSnap = await getDocs(userQuery);
+
+            if(userSnap.empty) {
+                throw new Error('Usuario no encontrado');
+            }
+            return userSnap.docs[0].data();
+        }
+        catch(err) {
+            throw new Error(err as string);
+        }
     }
 
     public static async createUser(name: string, email: string, password: string, userType: string): Promise<string> {
-        const connection = await pool.getConnection();
         try {
-            await connection.beginTransaction();
 
-            const [newPerson] = await connection.query<ResultSetHeader>('INSERT INTO Person (name) VALUES (?)', [name]);
-
-            const idPerson: number = newPerson.insertId;
-
-            await connection.query('INSERT INTO User (email, password, userType, id_person) VALUES (?, ?, ?, ?)', [email, password, userType, idPerson]);
-
-            await connection.commit();
+            await addDoc(collection(db, "User"), {
+                email: email,
+                password: password,
+                userType: userType,
+                name: name,
+            });
 
             return 'Usuario creado exitósamente';
         }
         catch(err) {
-            connection.rollback();
             throw new Error(err as string);
-        }
-        finally {
-            connection.release();
         }
     }
 }
